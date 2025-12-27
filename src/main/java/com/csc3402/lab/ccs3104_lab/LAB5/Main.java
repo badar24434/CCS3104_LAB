d7a4b40d712577ee9bc1d124e597e4a0002ff958
badar24434 <164218215+badar24434@users.noreply.github.com>
2025-12-27 20:45:13 +0800

    Implement main method and utility functions in Main class

--- src/main/java/com/csc3402/lab/ccs3104_lab/LAB5/Main.java
+++ src/main/java/com/csc3402/lab/ccs3104_lab/LAB5/Main.java
@@ -1,4 +1,26 @@
 package com.csc3402.lab.ccs3104_lab.LAB5;
 
 public class Main {
+    public static void main(String[] args) {
+
+        sayHello();
+        addnumbers(5, 10,15);
+        printnumbers();
+    }
+
+    public static void sayHello() {
+        System.out.println("Welcome!");
+    }
+
+    public static void addnumbers(int a, int b, int c) {
+        int sum = a + b+ c;
+        System.out.println("The sum is: " + sum);
+    }
+
+    public static void  printnumbers() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
 }
