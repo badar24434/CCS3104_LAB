279d03705c7ac31ce9f15b296c7b3166e39d9d25
badar24434 <164218215+badar24434@users.noreply.github.com>
2025-12-29 00:47:17 +0800

    Add exports and opens for LAB6 module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,6 +11,7 @@ module com.csc.lab.ccs3104_lab {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires java.rmi;
+    requires java.sql;
 
     opens com.csc3402.lab.ccs3104_lab to javafx.fxml;
     exports com.csc3402.lab.ccs3104_lab;
@@ -24,6 +25,8 @@ module com.csc.lab.ccs3104_lab {
     opens com.csc3402.lab.ccs3104_lab.LAB4 to javafx.fxml;
     exports com.csc3402.lab.ccs3104_lab.LAB5;
     opens com.csc3402.lab.ccs3104_lab.LAB5 to javafx.fxml;
+    exports com.csc3402.lab.ccs3104_lab.LAB6;
+    opens com.csc3402.lab.ccs3104_lab.LAB6 to javafx.fxml;
     exports com.csc3402.lab.ccs3104_lab.PROJECT_EXAMPLE;
     opens com.csc3402.lab.ccs3104_lab.PROJECT_EXAMPLE to javafx.fxml;
 }
